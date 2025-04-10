4d6498e9c4cacda9eecb674a781eb853b1c874d0
timxu23 <timxu23@gmail.com>
2025-04-10 02:10:35 -0400

    added default env values in extensions

--- web-app/app/extensions.py
+++ web-app/app/extensions.py
@@ -13,7 +13,7 @@ from flask_login import LoginManager
 load_dotenv()
 
 # Initialize shared extensions
-mongo = pymongo.MongoClient(os.getenv("MONGO_URI"), ssl=True)
-db = mongo[os.getenv("MONGO_DBNAME")]
+mongo = pymongo.MongoClient(os.getenv("MONGO_URI", "mongodb://admin:secretpassword@localhost:27017/gesture_auth?authSource=admin"), ssl=True)
+db = mongo[os.getenv("MONGO_DBNAME", "test_db")]
 bcrypt = Bcrypt()
 login_manager = LoginManager()
